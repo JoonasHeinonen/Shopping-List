2700bf2679c28b743ab77f354c21b25574cc667a
Joonas Heinonen <joonas.heinonen@edu.turkuamk.fi>
2019-06-20 00:33:47 +0300

    The contact to database is succesful.

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,3 +1,8 @@
 <?php
+    $dbServername = 'localhost';
+    $dbUsername = 'root';
+    $dbPassword = '';
+    $dbName = 'shopping-list';
 
+    $conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
 ?>
--- index.php
+++ index.php
@@ -1,9 +1,24 @@
+<?php
+    include_once 'includes/dbh.inc.php';
+?>
+
 <!DOCTYPE html>
 <html>
     <head>
         <title>Shopping List</title>
     </head>
     <body>
-        <h1>Shopping List<h1>
+        <h1>Shopping List</h1>
+        <?php
+            $sql = "SELECT * FROM product;";
+            $result = mysqli_query($conn, $sql);
+            $resultCheck = mysqli_num_rows($result);
+
+            if ($resultCheck > 0) {
+                while ($row = mysqli_fetch_assoc($result)) {
+                    echo $row['product_name'] . ", " . $row['product_prize'] . " €<br>";
+                }
+            }
+        ?>
     <body>
 </html>
