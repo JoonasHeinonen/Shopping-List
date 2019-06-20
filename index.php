96e69ec023f6e3622839bfc989b1e6983df53ddd
Joonas Heinonen <joonas.heinonen@edu.turkuamk.fi>
2019-06-20 13:09:20 +0300

    Added a code to calculate the total prize.

--- index.php
+++ index.php
@@ -10,6 +10,9 @@
     <body>
         <h1>Shopping List</h1>
         <?php
+            $sum = 0;
+
+            // The code where to print out all the products with their prizes
             $sql = "SELECT * FROM product;";
             $result = mysqli_query($conn, $sql);
             $resultCheck = mysqli_num_rows($result);
@@ -19,6 +22,14 @@
                     echo $row['product_name'] . ", " . $row['product_prize'] . " €<br>";
                 }
             }
+            
+            // This is the code where the total prize is calculated
+            $sql_for_total = "SELECT SUM(product_prize) AS total_prize FROM product;";
+            $result_for_total = mysqli_query($conn, $sql_for_total);
+            $row = mysqli_fetch_assoc($result_for_total); 
+            $sum = $row['total_prize'];
+
+            echo "The total prize of these <b>" . $resultCheck ."</b> items would be <b>" . $sum . "</b> euros.";
         ?>
     <body>
 </html>
