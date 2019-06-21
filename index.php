a892b3a4c7e7c399213fab72699da55e7592d9fe
Joonas Heinonen <joonas.heinonen@edu.turkuamk.fi>
2019-06-21 12:07:32 +0300

    Added a form to add a new product.

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
 <html>
     <head>
         <title>Shopping List</title>
+        <link rel="stylesheet" type="text/css" href="style.css">
     </head>
     <body>
         <h1>Shopping List</h1>
@@ -13,8 +14,8 @@
             $sum = 0;
 
             // The code where to print out all the products with their prizes
-            $sql = "SELECT * FROM product;";
-            $result = mysqli_query($conn, $sql);
+            $sql_select_all = "SELECT * FROM product;";
+            $result = mysqli_query($conn, $sql_select_all);
             $resultCheck = mysqli_num_rows($result);
 
             if ($resultCheck > 0) {
@@ -30,6 +31,18 @@
             $sum = $row['total_prize'];
 
             echo "The total prize of these <b>" . $resultCheck ."</b> items would be <b>" . $sum . "</b> euros.";
+
+            /* INSERT INTO product (`product_name`, `product_prize`, `product_type`) VALUES ('Juhla Mokka 500g sj', 3.89, 3); */
+            $sql_add_into = "INSERT INTO product (`product_name`, `product_prize`, `product_type`) VALUES ('Juhla Mokka 500g sj', 3.89, 3);";
         ?>
+        <div class="form-box">
+            <form>
+                <h3>Submit a new product!</h3>
+                <input type="text" name="product-name" placeholder="The name of the product..."><br>
+                <input type="number" name="product-prize" step="0.01" placeholder="The price of the product..."><br>
+                <input type="number" name="product-type" placeholder="Product type..."><br>
+                <button type="submit" class="default-button" name="submit">Submit!</button>
+            </form>
+        </div>
     <body>
 </html>
